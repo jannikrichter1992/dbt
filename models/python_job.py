da745dd13bd6e82a7b26a87502cae647cfb30729
Jannik Richter <richter@office.weclapp.com>
2022-11-22 11:45:01 +0100

    dasds

--- models/python_job.py
+++ models/python_job.py
@@ -1,6 +1,6 @@
 def model(dbt, session):
 
-    my_sql_model_df = dbt.ref("country_language")
+    my_sql_model_df = dbt.ref("op_country_language")
     my_sql_model_df["test"] = "test"
     final_df = my_sql_model_df
 
